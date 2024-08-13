5a9ccf08dc70e9592ca3aee3a8ad28b28832a848
kenerry-serain <kserain.development@gmail.com>
2024-08-13 20:35:07 -0300

    [ECR] Uploading changes

--- variables.tf
+++ variables.tf
@@ -114,3 +114,18 @@ variable "eks_cluster" {
     }
   }
 }
+
+variable "ecr_repositories" {
+  type = list(object({
+    name                 = string
+    image_tag_mutability = string
+  }))
+
+  default = [{
+    name                 = "devops-na-nuvem-week/production/frontend"
+    image_tag_mutability = "MUTABLE"
+    }, {
+    name                 = "devops-na-nuvem-week/production/backend"
+    image_tag_mutability = "MUTABLE"
+  }]
+}
